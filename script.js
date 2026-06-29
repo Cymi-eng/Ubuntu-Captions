70740ad3a400c6aa3227022ed52e8e910935429f
cymi-eng <eaiancymi@gmail.com>
2026-06-29 16:51:31 +0300

    add scroll button functionality

--- script.js
+++ script.js
@@ -14,3 +14,26 @@ menuBtn.addEventListener("click", function() {
     // make the menu display vertically
     mobileMenu.classList.toggle("flex")
 });
+// Scroll To Top Button
+const topBtn = document.getElementById("topBtn");
+
+// Show button after scrolling 300px
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 300) {
+        topBtn.classList.remove("hidden");
+    } else {
+        topBtn.classList.add("hidden");
+    }
+
+});
+
+// Scroll smoothly to the top
+topBtn.addEventListener("click", () => {
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+});
